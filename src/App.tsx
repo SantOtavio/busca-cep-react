54f4737058e7e96d467bad64005027a2ed968ffd
SantOtavio <otavio.santos3006@gmail.com>
2022-05-23 21:44:12 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+interface cep {
+  localidade: String,
+  bairro: String;
+  logradouro: String;
+}
 
 function App() {
+  const [cepNumber, setCepNumber] = useState("");
+  const [cepData, setCepData] = useState<cep>();
+
+  const submmitHandler = () => {
+    // const formData = new FormData(e.target);
+    // const data = Object.fromEntries(formData);
+    fetch(`https://viacep.com.br/ws/${cepNumber}/json/`)
+      .then((response) => response.json())
+      .then((data: cep) => {
+        setCepData(data);
+        console.log(data);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Buscar CEP</h1>
+      <div className="form-group">
+        <input
+          type="text"
+          name="tracking"
+          className="form-control"
+          onChange={(e) => setCepNumber(e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        name="track-cep"
+        className="btn btn-primary"
+        onClick={submmitHandler}
+      >
+        Buscar
+      </button>
+      <br />
+      <br />
+      {cepData && <h1>Cidade: {cepData.localidade}</h1>}
+      {cepData && <h1>Bairro: {cepData.bairro}</h1>}
+      {cepData && <h1>Rua: {cepData.logradouro}</h1>}
     </div>
   );
 }
